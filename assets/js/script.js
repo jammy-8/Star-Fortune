fa3610d82f3637c6bd894a3842eab0849d0a16e9
jammy-8 <unb1eoh2@students.codeinstitute.net>
2025-03-05 09:41:40 +0000

    added choices and results on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-console.log('Aligned!')
+const compChoice = document.getElementById('computer-choice')
+const yourChoice = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const playButton = document.getElementById('manifest')
+const possibleChoices = ['fire', 'water', 'air', 'earth']
