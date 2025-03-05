350f97cc08a83d6f2a1f8097d79bebfaa0b6e9a9
jammy-8 <unb1eoh2@students.codeinstitute.net>
2025-03-05 09:49:05 +0000

    changed button and enabled it through js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,11 @@ const yourChoice = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const playButton = document.getElementById('manifest')
 const possibleChoices = ['fire', 'water', 'air', 'earth']
+
+// Function to generate a random choice for the computer //
+const choices = document.getElementsByName('choice')
+choices.forEach((c) => {
+    c.addEventListener('click', () => {
+        playButton.disabled = false
+    })
+})
