e242d6d288b584bb427f0b631069e744fd731915
jammy-8 <unb1eoh2@students.codeinstitute.net>
2025-03-05 14:37:29 +0000

    validated script.js added semicolons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,53 @@
-const compChoice = document.getElementById('computer-choice')
-const yourChoice = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const playButton = document.getElementById('manifest')
-const possibleChoices = ['fire', 'water', 'air', 'earth']
+const compChoice = document.getElementById('computer-choice');
+const yourChoice = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const playButton = document.getElementById('manifest');
+const possibleChoices = ['fire', 'water', 'air', 'earth'];
 
 // Function to generate a random choice for the computer //
-const choices = document.getElementsByName('choice')
+const choices = document.getElementsByName('choice');
 choices.forEach((c) => {
     c.addEventListener('click', () => {
-        playButton.disabled = false
-    })
-})
+        playButton.disabled = false;
+    });
+});
 
 playButton.addEventListener('click', (e) => {
-    const buttonsArray = Array.from(choices)
-    const selected = buttonsArray.filter((b) => b.checked)
-    addTextToSpan(yourChoice, selected[0].id)
-    const randChoice = generateComputerChoice()
-    showResult(selected[0].id, randChoice)
+    const buttonsArray = Array.from(choices);
+    const selected = buttonsArray.filter((b) => b.checked);
+    addTextToSpan(yourChoice, selected[0].id);
+    const randChoice = generateComputerChoice();
+    showResult(selected[0].id, randChoice);
     e.target.disabled = true;
     choices.forEach((b) => {
         b.checked = false;
-    })
-})
+    });
+});
 
 function addTextToSpan(spanControl, text) {
-    spanControl.textContent = text
+    spanControl.textContent = text;
 }
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
-    const computerChoice = possibleChoices[randomNumber]
-    addTextToSpan(compChoice, possibleChoices[randomNumber])
-    return computerChoice
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length);
+    const computerChoice = possibleChoices[randomNumber];
+    addTextToSpan(compChoice, possibleChoices[randomNumber]);
+    return computerChoice;
 }
 
 function showResult(userChoice, computerChoice) {   
-    let result
+    let result;
     if (userChoice === computerChoice) {
-        result = 'The Stars shines bright for you!'
+        result = 'The Stars shines bright for you!';
     } else if ((userChoice === 'fire' && computerChoice === 'water') ||
         (userChoice === 'water' && computerChoice === 'air') ||
         (userChoice === 'air' && computerChoice === 'earth') ||
         (userChoice === 'earth' && computerChoice === 'fire')) {
-        result = 'Universe Conspired!'
+        result = 'Universe Conspired!';
     } else {
-        result = 'Incompatible!'
+        result = 'Incompatible!';
     }
-    addTextToSpan(resultDisplay, result)
+    addTextToSpan(resultDisplay, result);
 }
 
 
